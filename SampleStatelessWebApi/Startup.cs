7e103547d357dead7cd29e594a27778229294285
Diego Martin <diegom@cubictelecom.com>
2020-10-14 09:49:17 +0200

    Added integration tests for aspnetcore 2.2

--- SampleStatelessWebApi/Startup.cs
+++ SampleStatelessWebApi/Startup.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using SampleStatelessWebApi.Services;
 
 namespace SampleStatelessWebApi
@@ -27,7 +21,7 @@ namespace SampleStatelessWebApi
         {
             services
                 .AddMvc()
-                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             services.AddTransient<IWeatherService, WeatherService>();
         }
---
+++ test/SampleStatelessWebApi.Tests/TestSupport/IntegrationTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SampleStatelessWebApi.Extensions;
+
+namespace SampleStatelessWebApi.Tests.TestSupport
+{
+    public abstract class IntegrationTest
+        : IDisposable
+    {
+        private readonly IServiceProvider _serviceProvider;
+        protected HttpClient HttpClient { get; }
+        protected IConfiguration Configuration { get; }
+        protected IntegrationTest()
+        {
+            var server =
+                new TestServer(
+                    new WebHostBuilder()
+                        .UseStartup<Startup>()
+                        .UseCommonConfiguration()
+                        .UseEnvironment("Test")
+                        .ConfigureTestServices(ConfigureTestServices));
+
+            HttpClient = server.CreateClient();
+            _serviceProvider = server.Host.Services;
+            Configuration = _serviceProvider.GetService<IConfiguration>();
+            Setup();
+        }
+
+        public void Dispose()
+        {
+            Cleanup();
+        }
+
+        protected T GetService<T>() where T: class
+        {
+            return _serviceProvider.GetService<T>();
+        }
+
+        protected virtual void ConfigureTestServices(IServiceCollection serviceCollection)
+        {
+        }
+
+        protected virtual void Cleanup()
+        {
+        }
+
+        protected abstract void Given();
+
+        protected abstract void When();
+
+        private void Setup()
+        {
+            Given();
+            When();
+        }
+    }
+}
